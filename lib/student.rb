0accae9ebfb2d0c880ac915ac38ceff480cc74b4
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-28 21:52:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,6 +89,7 @@ class Student
       SELECT * FROM students WHERE grade = 10 LIMIT 1
     SQL
     DB[:conn].execute(sql).map do |row|
+      binding.pry
       self.new_from_db(row)
     end
   end
